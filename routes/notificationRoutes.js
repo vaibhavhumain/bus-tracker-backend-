30749eb901a01bfb1fce8b73abb0cf4856e554c4
vaibhav <codweird5@gmail.com>
2025-07-07 10:34:18 +0530

    changes

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const { sendNotification } = require("../controllers/notificationController");
-const auth = require("../middleware/auth");
+const auth = require("../middleware/authMiddleware");
 
 router.post(
   "/send",
