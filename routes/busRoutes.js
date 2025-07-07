19fd5833d3694190eda36138669d50be5d643cef
vaibhav <codweird5@gmail.com>
2025-07-07 17:19:35 +0530

    asndfuoib

--- routes/busRoutes.js
+++ routes/busRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createBusOrder , getBusOrderById , getAllBusOrders , updateProgressStage , addProgressLog , uploadBusMedia , deleteBusOrder , generateAndSaveBusPdf} = require('../controllers/busController');
+const {createBusOrder , getBusOrderById , getAllBusOrders , updateProgressStage , addProgressLog , uploadBusMedia , deleteBusOrder , generateAndSaveBusPdf , downloadBusPdf} = require('../controllers/busController');
 const auth = require('../middleware/authMiddleware');
 const router = express.Router();
 
@@ -11,7 +11,7 @@ router.post('/:id/generate-pdf',auth ,generateAndSaveBusPdf);
 
 router.get('/all',auth,getAllBusOrders);
 router.get('/:id',auth,getBusOrderById);
-router.get('/download-pdf/:id', auth, busController.downloadBusPdf);
+router.get('/download-pdf/:id', auth, downloadBusPdf);
 
 router.put('/:id/progress',auth,updateProgressStage);
 
